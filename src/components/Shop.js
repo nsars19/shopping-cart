c9dc874ad084e5aaa67260bec4d4eb1e7a897257
Nick Sardonini <nsardonini@gmail.com>
2021-02-02 10:21:40 -0500

    add dynamic linking to each item
    items will be rendered with the ItemDetail component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import itemsList from "./itemsList";
 import Cart from "./Cart";
+import { Link } from "react-router-dom";
 
 const Shop = (props) => {
   const buildItemElement = (item) => {
     return (
       <div key={item.id}>
-        <h1>{item.name} - ${item.price}</h1>
+        <Link to={`/shop/${item.id}`}>
+          <h1>{item.name} - ${item.price}</h1>
+        </Link>
         <h3 onClick={() => props.handleClick(item)}>Add to Cart</h3>
       </div>
     )
