74f5bf1e1db8ceee681b31628f06d06cd7c41d9c
Nick Sardonini <nsardonini@gmail.com>
2021-02-02 12:54:43 -0500

    add navbar brand to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import "../styles/Nav.css";
 const Navbar = (props) => {
   return (
     <nav className="nav">
+      <span className="nav-brand">Cheese Guys</span>
       <span className="nav-item">
         <Link to="/">Home</Link>
       </span>
