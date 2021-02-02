e4b8f5da783749d509697268c25a4eb70b033de1
Nick Sardonini <nsardonini@gmail.com>
2021-02-02 10:20:31 -0500

    prettify CartItems.js

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -6,14 +6,23 @@ function CartItems(state) {
 
   return (
     <div>
-      {cart.map(item => {
-        return <p key={uniqid()}>{item.name} - ${item.price}</p>
+      {cart.map((item) => {
+        return (
+          <p key={uniqid()}>
+            {item.name} - ${item.price}
+          </p>
+        );
       })}
-      <h3>Total: ${cart.reduce((total, current) => {
-        return total + parseFloat(current.price);
-      }, 0).toFixed(2)}</h3>
+      <h3>
+        Total: $
+        {cart
+          .reduce((total, current) => {
+            return total + parseFloat(current.price);
+          }, 0)
+          .toFixed(2)}
+      </h3>
     </div>
-  )
+  );
 }
 
 export default CartItems;
