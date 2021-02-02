eca701641fef62b26f57c1483e831b6db8f970b8
Nick Sardonini <nsardonini@gmail.com>
2021-02-02 10:23:28 -0500

    add dynamic route for show items
    each shop item is rendered from the ItemDetail component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Shop from "./components/Shop";
-import Cart from "./components/Cart";
-import itemsList from "./components/itemsList";
-import ItemInfo from "./components/ItemInfo";
 import CartItems from "./components/CartItems";
+import ItemDetail from "./components/ItemDetail";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -24,6 +22,12 @@ function App() {
           path="/shop"
           render={() => <Shop cart={cart} handleClick={addToCart} />}
         />
+        <Route
+          path={"/shop/:itemId"}
+          render={(props) => {
+            return <ItemDetail {...props} cart={cart} handleClick={addToCart} />;
+          }}
+        />
         <Route path="/cartitems" component={CartItems} />
         <Route path="/" component={Homepage} />
       </Switch>
@@ -32,4 +36,3 @@ function App() {
 }
 
 export default App;
-
