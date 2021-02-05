af06832390d23106428e7a40f93d808c73bce1a8
Nick Sardonini <nsardonini@gmail.com>
2021-02-05 15:02:00 -0500

    add wrapper around nav-items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,19 @@ const Navbar = (props) => {
   return (
     <nav className="nav">
       <span className="nav-brand">Cheese Guys</span>
-      <span className="nav-item">
-        <Link to="/">Home</Link>
-      </span>
-      <span className="nav-item">
-        <Link to="/shop">Shop</Link>
-      </span>
-      <span className="nav-item">
-        <Link to={{
-          pathname: "/cartitems",
-          state: props.cart,
-        }}>Cart</Link>
+      <span className="nav-items">
+        <span className="nav-item">
+          <Link to="/">Home</Link>
+        </span>
+        <span className="nav-item">
+          <Link to="/shop">Shop</Link>
+        </span>
+        <span className="nav-item">
+          <Link to={{
+            pathname: "/cartitems",
+            state: props.cart,
+          }}>Cart({props.cart.length})</Link>
+        </span>
       </span>
     </nav>
   );
