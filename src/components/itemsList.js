151dfe07d4dffcc4a9b608d5bda9cbba5969a6f6
Nick S <nsardonini@gmail.com>
2021-02-08 12:38:13 -0500

    change placeholder items, add images to item objects

--- src/components/itemsList.js
+++ src/components/itemsList.js
@@ -1,43 +1,60 @@
+import gruyere from "../assets/gruyere.jpeg";
+import bleu from "../assets/bleu.jpeg";
+import brie from "../assets/brie.jpeg";
+import camembert from "../assets/camembert.jpeg";
+import mascarpone from "../assets/mascarpone.jpeg";
+import roquefort from "../assets/roquefort.jpeg";
+import gouda from "../assets/gouda.jpeg";
+import jarlsberg from "../assets/jarlsberg.jpeg";
+
 const itemsList = {
   items: [
     {
-      name: "Milk",
-      price: "1.99",
+      name: "Gruyere",
+      price: "5.99/lb",
+      src: gruyere,
       id: 0,
     },
     {
-      name: "Eggs",
-      price: ".99",
+      name: "Bleu",
+      price: "4.99/lb",
+      src: bleu,
       id: 1,
     },
     {
-      name: "Bread",
-      price: "2.99",
+      name: "Brie",
+      price: "5.99/lb",
+      src: brie,
       id: 2,
     },
     {
-      name: "Ham",
-      price: "3.99",
+      name: "Camembert",
+      price: "8.99/lb",
+      src: camembert,
       id: 3,
     },
     {
-      name: "Peanut Butter",
-      price: "4.99",
+      name: "Mascarpone",
+      price: "12.99/lb",
+      src: mascarpone,
       id: 4,
     },
     {
-      name: "Pizza",
-      price: "7.99",
+      name: "Roquefort",
+      price: "15.99/lb",
+      src: roquefort,
       id: 5,
     },
     {
-      name: "Chicken Tenders",
-      price: "6.99",
+      name: "Gouda",
+      price: "7.99/lb",
+      src: gouda,
       id: 6,
     },
     {
-      name: "Honey Mussy",
-      price: "3.99",
+      name: "Jarlsberg",
+      price: "8.99/lb",
+      src: jarlsberg,
       id: 7,
     },
   ],
