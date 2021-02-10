47a8ebc2e3ae5bdbda436c21165e805fd062f64f
Nick Sardonini <nsardonini@gmail.com>
2021-02-10 17:01:04 -0500

    add item descriptions

--- src/components/itemsList.js
+++ src/components/itemsList.js
@@ -14,48 +14,56 @@ const itemsList = {
       price: "5.99",
       src: gruyere,
       id: 0,
+      desc: "Halloumi ricotta lancashire. Cheeseburger fromage frais say cheese red leicester rubber cheese mozzarella manchego halloumi. Brie parmesan swiss mascarpone say cheese gouda mozzarella st. agur blue cheese. Say cheese feta.Chalk and cheese brie who moved my cheese. Ricotta jarlsberg swiss cheddar cheese and biscuits taleggio macaroni cheese lancashire. Hard cheese cheese and biscuits cheesy grin cheese triangles everyone loves goat boursin chalk and cheese. Brie gouda dolcelatte port-salut chalk and cheese mozzarella say cheese croque monsieur. Hard cheese."
     },
     {
       name: "Bleu",
       price: "4.99",
       src: bleu,
       id: 1,
+      desc: "When the cheese comes out everybody's happy squirty cheese caerphilly. Say cheese jarlsberg cheesy feet bocconcini fromage bocconcini danish fontina queso. Paneer parmesan queso everyone loves cream cheese cheese and biscuits cow cheeseburger. The big cheese paneer cream cheese cow cheesy feet manchego cream cheese red leicester. Brie smelly cheese ricotta."
     },
     {
       name: "Brie",
       price: "5.99",
       src: brie,
       id: 2,
+      desc: "Port-salut roquefort gouda. Croque monsieur cut the cheese swiss smelly cheese blue castello feta swiss mozzarella. Monterey jack cow macaroni cheese brie cheesy grin jarlsberg roquefort cheddar. Pepper jack fondue cheeseburger roquefort cheeseburger."
     },
     {
       name: "Camembert",
       price: "8.99",
       src: camembert,
       id: 3,
+      desc: "Mozzarella say cheese halloumi. Red leicester pepper jack goat babybel st. agur blue cheese mascarpone cheese slices paneer. Babybel camembert de normandie pepper jack roquefort pecorino ricotta cheese strings red leicester. Cheesy feet airedale fromage cheese and biscuits gouda smelly cheese brie cheddar. Cow monterey jack."
     },
     {
       name: "Mascarpone",
       price: "12.99",
       src: mascarpone,
       id: 4,
+      desc: "Queso mascarpone ricotta. Emmental cauliflower cheese lancashire hard cheese camembert de normandie everyone loves who moved my cheese port-salut. Cut the cheese rubber cheese stilton fromage frais say cheese brie cow caerphilly. Brie roquefort chalk and cheese gouda blue castello babybel."
     },
     {
       name: "Roquefort",
       price: "15.99",
       src: roquefort,
       id: 5,
+      desc: "Cheesy grin rubber cheese goat. Fromage cream cheese cheeseburger blue castello danish fontina croque monsieur pepper jack boursin. Cut the cheese stinking bishop cauliflower cheese parmesan fromage hard cheese cheesy grin gouda. Blue castello pepper jack when the cheese comes out everybody's happy parmesan goat cheesy grin fromage cheesecake. Cheddar pepper jack."
     },
     {
       name: "Gouda",
       price: "7.99",
       src: gouda,
       id: 6,
+      desc: "Manchego mozzarella cottage cheese. Everyone loves jarlsberg when the cheese comes out everybody's happy stinking bishop jarlsberg halloumi roquefort paneer. Macaroni cheese who moved my cheese cheese strings roquefort cheeseburger rubber cheese the big cheese who moved my cheese. Cheese triangles mozzarella."
     },
     {
       name: "Jarlsberg",
       price: "8.99",
       src: jarlsberg,
       id: 7,
+      desc: "Cheese strings paneer paneer. Goat macaroni cheese fromage bavarian bergkase cut the cheese cheddar croque monsieur manchego. Gouda st. agur blue cheese stinking bishop say cheese port-salut mascarpone cow goat. Queso jarlsberg cheeseburger cheese strings the big cheese danish fontina cheeseburger cheddar. Cheesy feet queso cheese strings cheesy feet."
     },
   ],
 }
