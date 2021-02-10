b66260bd73ade7f0460e65be2fd2ec7e80124c98
Nick Sardonini <nsardonini@gmail.com>
2021-02-10 14:55:44 -0500

    add change handling function for input element

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,31 @@ import { Link } from "react-router-dom";
 import uniqid from "uniqid";
 
 const Cart = (props) => {
+  // Get unique cart items for card elements
+  const cart = new Set(props.cart);
+
+  const count = (itemToCount) => {
+    return props.cart.filter((item) => item === itemToCount).length;
+  };
+
+  const handleChange = (item, e) => {
+    const value = parseInt(e.target.value);
+    let diff = value - count(item);
+
+    if (diff === 0) return;
+
+    let itemsToAdd = [];
+    for (let i = 0; i < Math.abs(diff); i++) {
+      if (diff > 0) {
+        itemsToAdd.push(item);
+      } else {
+        props.removeFromCart(item);
+      }
+    }
+
+    if (!!itemsToAdd.length) props.setCart([...cart, ...itemsToAdd]);
+  };
+
   return (
     <div>
       {props.cart.map((item) => {
